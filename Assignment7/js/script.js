580d662a2b50b63afd24b82660c6efeca564de19
Anusree k k <anusree.kk@coddletech.com>
2023-09-19 18:12:21 +0530

    Assignment corrected

--- Assignment7/js/script.js
+++ Assignment7/js/script.js
@@ -3,89 +3,73 @@
 /* eslint-disable semi */
 
 /* eslint-disable quotes */
-let currentPlayer = "X"
-let main_row = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-];
+let currentPlayer = "X";
+let column1 = document.querySelector(".column1");
+let column2 = document.querySelector(".column2");
+let column3 = document.querySelector(".column3");
+let column4 = document.querySelector(".column4");
+let column5 = document.querySelector(".column5");
+let column6 = document.querySelector(".column6");
+let column7 = document.querySelector(".column7");
+let column8 = document.querySelector(".column8");
+let column9 = document.querySelector(".column9");
 
-const message = document.getElementById("showmessage")
-const columns = document.querySelectorAll(".column")
+const message = document.getElementById("showmessage");
+const columns = [column1, column2, column3, column4, column5, column6, column7, column8, column9];
 
 for (let index = 0; index < columns.length; index++) {
     columns[index].addEventListener("click", () => {
-        const row = Math.floor(index / 3)
-        const col = index % 3
-
-        if (main_row[row][col] === "" && !gamestop()) {
-            main_row[row][col] = currentPlayer
-            columns[index].textContent = currentPlayer
-            currentPlayer = currentPlayer === "X" ? "O" : "X"
+        if (columns[index].textContent === "" && !gamestop()) {
+            columns[index].textContent = currentPlayer;
+            currentPlayer = currentPlayer === "X" ? "O" : "X";
             updateMessage();
         }
     });
 }
 
 function gamestop() {
-    return checkwinner("X") || checkwinner("O") || boardIsFull()
+    return checkwinner("X") || checkwinner("O") || boardIsFull();
 }
 
 function boardIsFull() {
-    for (let row = 0; row < 3; row++) {
-        for (let col = 0; col < 3; col++) {
-            if (main_row[row][col] === '') {
-                return false;
-            }
+    for (let index = 0; index < columns.length; index++) {
+        if (columns[index].textContent === "") { 
+            return false;
         }
     }
-    return true
+    return true;
 }
-
 function checkwinner(player) {
-    for (let i = 0; i < 3; i++) {
-        if (main_row[i][0] === player && main_row[i][1] === player && main_row[i][2] === player) {
-            return true
-        }
-        if (main_row[0][i] === player && main_row[1][i] === player && main_row[2][i] === player) {
-            return true
-        }
+    if ((column1.textContent === player && column2.textContent === player && column3.textContent === player) ||
+        (column4.textContent === player && column5.textContent === player && column6.textContent === player) ||
+        (column7.textContent === player && column8.textContent === player && column9.textContent === player) ||
+        (column1.textContent === player && column4.textContent === player && column7.textContent === player) ||
+        (column1.textContent === player && column5.textContent === player && column9.textContent === player) ||
+        (column2.textContent === player && column5.textContent === player && column8.textContent === player) ||
+        (column3.textContent === player && column6.textContent === player && column9.textContent === player) ||
+        (column3.textContent === player && column5.textContent === player && column7.textContent === player)) {
+        return true;
     }
-    if (main_row[0][0] === player && main_row[1][1] === player && main_row[2][2] === player) {
-        return true
-    }
-    if (main_row[0][2] === player && main_row[1][1] === player && main_row[2][0] === player) {
-        return true
-    }
-    return false
+    return false;
 }
-
 function updateMessage() {
-    if (checkwinner('X')) {
-        message.textContent = "Player X wins"
+    if (checkwinner("X")) {
+        message.textContent = "Player X wins";
     } else if (checkwinner("O")) {
-        message.textContent = "Player O wins"
+        message.textContent = "Player O wins";
     } else if (boardIsFull()) {
-        message.textContent = 'It\'s a draw'
+        message.textContent = "It's a draw";
     } else {
-        message.textContent = `Player ${currentPlayer}'s turn`
+        message.textContent = `Player ${currentPlayer}'s turn`;
     }
 }
-const resetbutton = document.getElementById("resetbutton")
-
+const resetbutton = document.getElementById("resetbutton");
 resetbutton.addEventListener("click", function () {
-    resetBoard()
+    resetBoard();
 });
-
 function resetBoard() {
-    main_row = [
-        ['', '', ''],
-        ['', '', ''],
-        ['', '', '']
-    ];
-    document.querySelectorAll(".column").forEach(cell => cell.textContent = "")
-
+    currentPlayer = "X";
+    columns.forEach((column) => {
+        column.textContent = "";
+    });
 }
-
-
-
